da0eaed35a6c54e42d48f8bc88785757c6da5d82
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-03-16 17:00:51 -0700

    working on login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,14 +28,23 @@ router.get('/', (req, res) => {
     })
       .then(dbPostData => {
         // pass a single post object into the homepage template
-        console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
+        
         res.render('homepage', { posts });
       })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+    });
+    
+    router.get('/login', (req, res) => {
+        if (req.session.loggedIn) {
+          res.redirect('/');
+          return;
+        }
+      
+        res.render('login');
+      });
 
 module.exports = router;
